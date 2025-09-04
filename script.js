9294174afa3dd277cac8d7dd977d496b01764482
Péter Husztek <phusztek@gmail.com>
2025-09-04 17:19:26 +0200

    Add backend

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('orderForm').addEventListener('submit', function(e){
-    e.preventDefault(); // ne töltse újra az oldalt
-
-    const product = document.getElementById('product').value;
-    const quantity = document.getElementById('quantity').value;
-
-    alert(`Rendelt termék: ${product}\nMennyiség: ${quantity}`);
-});
