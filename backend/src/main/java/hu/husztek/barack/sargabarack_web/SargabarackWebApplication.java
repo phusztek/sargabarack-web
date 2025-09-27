cbb5aa8a509463b35758be6fa665bec077fda911
Péter Husztek <phusztek@gmail.com>
2025-09-27 17:40:52 +0200

    Add bckend

---
+++ backend/src/main/java/hu/husztek/barack/sargabarack_web/SargabarackWebApplication.java
@@ -0,0 +1,13 @@
+package hu.husztek.barack.sargabarack_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SargabarackWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SargabarackWebApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hu/husztek/barack/sargabarack_web/SargabarackWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.husztek.barack.sargabarack_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SargabarackWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
